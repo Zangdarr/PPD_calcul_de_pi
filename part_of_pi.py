6cd798e0ac406a2d5f16edb601cfd1806d4aacbb
Verkyndt Alexandre <alexandre.verkyndt@gmail.com>
2015-03-18 16:15:28 +0100

    Création de part_of_pi.py
    Ce fichier calcul une partie de pi en fonction du nombre d'unité de calcul et de la précision voulu.

---
+++ part_of_pi.py
@@ -0,0 +1,25 @@
+"""
+Calcul d'une partie du nombre pi
+Stockera le resultat dans un fichier resultats.tmp
+"""
+import sys
+
+if __name__ == "__main__" :
+    starting_value = int(sys.argv[1])
+    step_value = int(sys.argv[2])
+    max_value = int(sys.argv[3])
+    FILE_TMP = "tmp_result" + str(starting_value)
+
+
+    print("START from {0} with {1} steps".format(starting_value,step_value))
+    
+    result = 0
+    i = starting_value
+    while(i<max_value):
+        etape1 = (-1)**i
+        etape2 = (2 * i) + 1
+        result = result + (etape1 / etape2)
+        i = i + step_value
+    
+    f = open(FILE_TMP, "a")
+    f.write("%s\n" % result)
