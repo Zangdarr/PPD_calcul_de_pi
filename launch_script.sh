e29db59da8657bfd37167acedaba368a97d343b5
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-18 17:07:40 +0100

    Ajout dans launch_script.sh
    Ajout de la boucle qui lance les calculs dans les noeuds.

--- launch_script.sh
+++ launch_script.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 #script de lancement du calcul de pi en parallèle
+
+machines=`cat $OAR_FILE_NODES`
+nb_machines=`wc -l $OAR_FILE_NODES`
+i=0
+max=1000000
+
+for m in $machines;do
+    oarsh $m echo `python3 part_of_pi.py $i $nb_machines $max > result_tmp` &
+    $i=$i + 1
+done
